f1359c7ccc21514060b2496cd0e7b9cf8fbb3178
Arsenii Yamnii <ayamnii@eyeconweb.com>
2021-06-17 10:42:06 +0500

    add hard and soft installation

--- install.sh
+++ install.sh
@@ -4,12 +4,20 @@ if [ "$1" == "--uninstall" ]; then
 	sudo rm /usr/bin/pycam.sh
 	sudo rm /usr/bin/pycam-service
 	sudo rm ~/.config/systemd/user/pycam.service
-else
-	touch ~/.owner.pickle
+if [ "$1" == "--hard" ]; then
+	sudo touch ~/.owner.pickle
 	sudo cp pycam.sh /usr/bin/pycam.sh
 	sudo cp main.py /usr/bin/pycam-service
 	mkdir -p ~/.config/systemd/user/
 	sudo cp pycam.service ~/.config/systemd/user/pycam.service
+	systemctl --user enable pycam.service
+	systemctl --user start pycam.service
+else
+	sudo touch ~/.owner.pickle
+	sudo ln pycam.sh /usr/bin/pycam.sh
+	sudo ln main.py /usr/bin/pycam-service
+	mkdir -p ~/.config/systemd/user/
+	sudo ln pycam.service ~/.config/systemd/user/pycam.service
 	systemctl --user enable pycam.service
 	systemctl --user start pycam.service
 fi
